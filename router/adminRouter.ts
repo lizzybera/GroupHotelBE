3e9ef59838af5134d547c4632af9da3a6ac9ec5f
unknown <fashkenny>
2023-08-23 13:47:13 +0100

    adding new files by kenny

--- router/adminRouter.ts
+++ router/adminRouter.ts
@@ -2,13 +2,11 @@ import express from "express";
 import upload from "../config/multer"
 import { createAdmin, signInAdmin, viewAdmins, viewOneAdmin } from "../controller/adminController";
 
-
-
 const router = express.Router();
 router.route("/createAdmin").post(upload, createAdmin)
 router.route("/signInAdmin").post(signInAdmin);
-router.route("/viewAdmins").post(viewAdmins);
-router.route("/:adminID/viewOneAdmin").post(viewOneAdmin);
+router.route("/viewAdmins").get(viewAdmins);
+router.route("/:adminID/viewOneAdmin").get(viewOneAdmin);
 
 
 export default router;
