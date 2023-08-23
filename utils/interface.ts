7ea906779e874d3405ad014f59eb778462558d79
umeh emmanuel <eumeh3882@gmail.com>
2023-08-23 02:35:43 -0700

    pushed by umeh

--- utils/interface.ts
+++ utils/interface.ts
@@ -9,3 +9,18 @@ interface iAuthor {
 }
 
 export interface iAuthorData extends iAuthor, mongoose.Document {}
+
+
+export interface iUser {
+    fullName?: string;
+    email?: string;
+    password?: string;
+    confirmPassword?: string;
+    city?: string;
+    phoneNumber?: number;
+    localGovernment?: string;
+    avatar?: string;
+    avatarUrl?: string;
+  }
+  
+  export interface iUserData extends iUser, mongoose.Document {}
