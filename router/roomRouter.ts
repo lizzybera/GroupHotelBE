c02acb2465a20b3a8c2012fe642ee7237faea780
unknown <fashkenny>
2023-08-23 14:49:20 +0100

    adding new files by kenny

--- router/roomRouter.ts
+++ router/roomRouter.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { createRoom, viewOneRoom, viewRooms } from "../controller/roomController";
+import upload from "../config/multer";
 
 
 const router = express.Router()
-router.route("createRoom").post(createRoom)
+router.route("createRoom").post(upload,createRoom)
 router.route("viewRooms").get(viewRooms);
 router.route("/:roomID/view").get(viewOneRoom);
 
